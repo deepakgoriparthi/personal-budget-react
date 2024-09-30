cee03297462c550e79d7f5e137f7a69502755233
deepakgoriparthi <deepakgoriparthi@gmail.com>
2024-09-29 22:33:31 -0400

    added all instructions

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.scss';
+
+import{
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import Menu from './Menu/Menu';
+import Hero from './Hero/Hero';
+import HomePage from './HomePage/HomePage';
+import Footer from './Footer/Footer';
+import AboutPage from './AboutPage/AboutPage';
+import LoginPage from './LoginPage/LoginPage';
+function App() {
+  return (
+    <Router>
+      <Menu/>
+      <Hero/>
+      <div className="mainContainer">
+        <Routes>
+          <Route path="/about" element={<AboutPage/>}>
+          </Route>
+          <Route path="/login" element={<LoginPage/>}>
+          </Route>
+          <Route path="/" element={<HomePage/>}>
+          </Route>
+        </Routes>
+      </div>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
